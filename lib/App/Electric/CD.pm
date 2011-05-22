d852acb74dcd139e0017bc19b69f187510ea1c20
Zakariyya Mughal <zaki.mughal@gmail.com>
2011-05-22 17:47:44 -0500

    Small changes to logging

--- lib/App/Electric/CD.pm
+++ lib/App/Electric/CD.pm
@@ -6,8 +6,10 @@ use warnings;
 use App::Electric::Window;
 use Log::Log4perl;
 
-#my $log_file = 'debug.log';
-my $log_file = '/dev/null';
+my $log_file = "$ENV{HOME}/.ecd_debug.log";
+#my $log_file = '/dev/null';
+#my $level = "INFO";
+my $level = "OFF";
 
 my $log_conf_debug = qq/
 	log4perl.category = INFO, Logfile
@@ -15,16 +17,16 @@ my $log_conf_debug = qq/
 	log4perl.appender.Logfile = Log::Log4perl::Appender::File
 	log4perl.appender.Logfile.filename = $log_file
 	log4perl.appender.Logfile.mode = write
-	log4perl.appender.Logfile.create_at_logtime = 0
+	log4perl.appender.Logfile.create_at_logtime = 1
 	log4perl.appender.Logfile.layout = Log::Log4perl::Layout::PatternLayout
 	log4perl.appender.Logfile.layout.ConversionPattern = %d %p> %m (%M)%n
 
-	log4perl.logger.main = OFF
-	log4perl.logger.Window = OFF
-	log4perl.logger.Component = OFF
-	log4perl.logger.HManager = OFF
-	log4perl.logger.EditingComponent = OFF
-	log4perl.logger.ListComponent = OFF
+	log4perl.logger.main = $level
+	log4perl.logger.Window = $level
+	log4perl.logger.Component = $level
+	log4perl.logger.HManager = $level
+	log4perl.logger.EditingComponent = $level
+	log4perl.logger.ListComponent = $level
 
 	log4perl.threshold = OFF
 /;
